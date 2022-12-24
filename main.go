a835f6d323c79df80afd972d6092a8c0f71474c1
Evgenii Michurin <anaxaim@gmail.com>
2022-12-25 00:21:15 +0400

    added viper

--- main.go
+++ main.go
@@ -4,29 +4,29 @@ import (
 	"database/sql"
 	"log"
 
+	_ "github.com/lib/pq"
+
 	"github.com/anaxaim/bank_api/api"
 	db "github.com/anaxaim/bank_api/db/sqlc"
-	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
+	"github.com/anaxaim/bank_api/util"
 )
 
 func main() {
-
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
 	if err != nil {
-		log.Fatal("cannot connect to db:", err)
+		log.Fatal("cannot load config: ", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
 	}
 
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
-		log.Fatal("cannot start server:", err)
+		log.Fatal("cannot start server: ", err)
 	}
 }
