3056b19b16c06ab364fca12ab8b31755109b698a
Evgenii Michurin <anaxaim@gmail.com>
2022-12-24 23:51:57 +0400

    api server
    - added HTTP server using gin
    - added firsts routes
    - fixed sqlc param to emit_empty_slices

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/anaxaim/bank_api/api"
+	db "github.com/anaxaim/bank_api/db/sqlc"
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:secret@localhost:5432/bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+}
